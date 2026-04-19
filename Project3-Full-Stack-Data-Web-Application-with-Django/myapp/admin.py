a289631d2e497bb22252620a227829578d33046b
nwalker9 <nicowalker29@gmail.com>
2026-04-19 11:13:28 -0400

    Create admin.py
    Created, and added code to admin.py

---
+++ Project3-Full-Stack-Data-Web-Application-with-Django/myapp/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import City, WeatherRecord, DataRun
+ 
+ 
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'latitude', 'longitude']
+    search_fields = ['name']
+ 
+ 
+@admin.register(WeatherRecord)
+class WeatherRecordAdmin(admin.ModelAdmin):
+    list_display   = ['city', 'date', 'temperature_max', 'temperature_min',
+                      'precipitation_sum', 'wind_speed_max', 'source']
+    list_filter    = ['city', 'source']
+    search_fields  = ['city__name']
+    ordering       = ['-date']
+ 
+ 
+@admin.register(DataRun)
+class DataRunAdmin(admin.ModelAdmin):
+    list_display  = ['city', 'run_timestamp', 'records_fetched', 'source']
+    list_filter   = ['city', 'source']
+    ordering      = ['-run_timestamp']
+ 
