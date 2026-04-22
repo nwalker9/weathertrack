8d7792e2b258accbc234c75bd6d09384af6168b9
nwalker9 <nicowalker29@gmail.com>
2026-04-22 17:05:00 -0400

    Update urls.py
    Fix config urls.py to use myapp routes

--- Project3-Full-Stack-Data-Web-Application-with-Django/config/urls.py
+++ Project3-Full-Stack-Data-Web-Application-with-Django/config/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path
-from . import views
-from .views import TaskDetailView, TaskListView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('records/', views.record_list, name='record_list'),
-    path('records/<int:pk>/', views.record_detail, name='record_detail'),
-    path('records/add/', views.record_create, name='record_create'),
-    path('records/<int:pk>/edit/', views.record_update, name='record_update'),
-    path('records/<int:pk>/delete/', views.record_delete, name='record_delete'),
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
 ]
