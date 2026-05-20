ca6be4d82262035d636302ffd1ee6ee88e1b8fe1
Nicolas Walker <nicowalker29@gmail.com>
2026-05-20 10:37:07 -0400

    Add player URLs

--- Project3-Full-Stack-Data-Web-Application-with-Django/myapp/urls.py
+++ Project3-Full-Stack-Data-Web-Application-with-Django/myapp/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('records/<int:pk>/delete/', views.record_delete, name='record_delete'),
     path('analytics/', views.analytics, name='analytics'),
     path('fetch/', views.fetch_data, name='fetch_data'),
+    path('players/', views.player_list, name='player_list'),
+    path('players/<int:pk>/', views.player_detail, name='player_detail'),
+    path('players/analytics/', views.player_analytics, name='player_analytics'),
 ]
